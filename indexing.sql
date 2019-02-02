5043e41b10c7abfd21608c0cb35248992cf9b851
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-02-01 16:05:17 -1000

    Query SELECT DISTINCT to search for `NISSAN GT-R`

--- indexing.sql
+++ indexing.sql
@@ -23,3 +23,8 @@ WITH OWNER indexed_cars_user;
 
 SELECT DISTINCT make_title FROM car_models
 WHERE make_code = 'LAM';
+
+-- Run a query to list all `model_title` values where the `make_code` is `'NISSAN'`, and the `model_code` is `'GT-R'` without any duplicate rows, and note the time somewhere. (should have 1 result)
+
+SELECT DISTINCT model_title FROM car_models
+WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
