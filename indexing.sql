412e151b1e653064a2d330062b99b238058620bb
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-02-01 16:11:08 -1000

    Select all make, model, title, year for lamborghinis

--- indexing.sql
+++ indexing.sql
@@ -28,3 +28,7 @@ WHERE make_code = 'LAM';
 
 SELECT DISTINCT model_title FROM car_models
 WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
+
+SELECT make_code, model_code, model_title, year
+FROM car_models
+WHERE make_code = 'LAM';
